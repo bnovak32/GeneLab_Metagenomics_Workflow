9cffb3a0f2cacfd2f629115e30cadfaead22c7ab
olabiyi <obadbotanist@yahoo.com>
2026-05-28 15:32:58 -0700

    fixed decontam outfile name and updated md5sums files

--- bin/generate_md5sums.py
+++ bin/generate_md5sums.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# EXAMPLE USAGE:
+# generate_md5sums.py --assay-suffix _GLlbsMetag --outdir processing/
+
 from email import parser
 import os
 import sys
@@ -23,17 +26,6 @@ def calculate_md5(filepath):
         sys.stderr.write(f"Error calculating MD5 for {filepath}: {str(e)}\n")
         return "ERROR"
 
-def is_raw_file(filepath):
-    """Check if file is a raw FASTQ or raw multiqc file (zip or html)."""
-    if "/Merged_Sequence_Data/" in filepath:
-        # Match raw fastq files
-        if filepath.endswith(".fastq.gz"):
-            return True
-        # Match raw multiqc reports (zip or html)
-        elif "raw_multiqc" in filepath and (filepath.endswith(".zip") or filepath.endswith(".html")):
-            return True
-    return False
-
 def should_include(filepath, allowed_dirs):
     """Check if file should be included in MD5 calculation."""
     
@@ -48,12 +40,17 @@ def should_include(filepath, allowed_dirs):
     # Skip any individual log files
     if ("/Logs/" in filepath and filepath.endswith(".log")):
         return False
-    
-    # Skip any files with 'fastqc' in the path or filename (case-sensitive to make sure Post_Processing/FastQC_Outputs/ is still included) 
-    if 'fastqc' in filepath:
+
+    # Skip all fastq files that are not in HR-removed_Sequence_Data or HostRm-removed_Sequence_Data
+    if (filepath.endswith(".fastq.gz") and ( ( "/HR-removed_Sequence_Data/" not in filepath) or ("/HostRm-removed_Sequence_Data/" not in filepath ) )  ):
         return False
 
-    # Skip all .png files in alpha Barplot outputs
+
+    # Skip any files with 'fastqc' in the path or temporarary files ending with .tmp 
+    if ('fastqc' in filepath) or filepath.endswith(".tmp"):
+        return False
+
+    # Skip all .png files in Barplot outputs
     if ("/Barplots/" in filepath and filepath.endswith(".png")):
         return False
 
@@ -62,7 +59,7 @@ def should_include(filepath, allowed_dirs):
         return False
 
     # Skip files in Post_Processing except for README and processing_info.zip
-    if "/Post_Processing/" in filepath and not (filepath.endswith("README" + args.assay_suffix + ".txt") or filepath.endswith("processing_info" + args.assay_suffix + ".zip")):
+    if "/Post_Processing/" in filepath and not (filepath.endswith(f"README{args.assay_suffix}.txt") or filepath.endswith(f"processing_info{args.assay_suffix}.zip")):
         return False
     
     return True
@@ -80,9 +77,8 @@ def trim_to_allowed_dir(filepath, allowed_dirs):
 def main():
     parser = argparse.ArgumentParser(description='Generate MD5 sum files for GeneLab data.')
     parser.add_argument('--outdir', required=True, help='Output directory containing files to process')
-    parser.add_argument('--assay_suffix', default='', help="GeneLab assay suffix. one of '_GLmetagenomics', '_GLlbsMetag', '_GLlblMetag', and '_GLlongMetag' (default: empty)")
-    parser.add_argument('--output_prefix', default='', help='Prefix for output MD5 sum files (default: empty)')
-    parser.add_argument('--generate_raw_md5sums', action='store_true', help='Generate raw MD5sums')
+    parser.add_argument('--assay-suffix', default='', help="GeneLab assay suffix. one of '_GLmetagenomics', '_GLlbsMetag', '_GLlblMetag', and '_GLlongMetag' (default: empty)")
+    parser.add_argument('--output-prefix', default='', help='Prefix for output MD5 sum files (default: empty)')
     
     global args
     args = parser.parse_args()
@@ -91,11 +87,6 @@ def main():
     outdir = os.path.abspath(args.outdir)
     
     # Create output files and initialize them without headers
-    if args.generate_raw_md5sums:
-        raw_md5_file = f"{args.output_prefix}raw_md5sum{args.assay_suffix}.tsv"
-        with open(raw_md5_file, 'w') as f:
-            f.write("File Path\tFile Name\tmd5\n")
-    
     processed_md5_file = f"{args.output_prefix}processed_md5sum{args.assay_suffix}.tsv"
     with open(processed_md5_file, 'w') as f:
         f.write("File Path\tFile Name\tmd5\n")
@@ -132,24 +123,12 @@ def main():
             basename = os.path.basename(filepath)
             trimmed_path = trim_to_allowed_dir(filepath, allowed_dirs)
             
-            # Generate and save md5sum of raw files only if enabled
-            if is_raw_file(filepath):
-                if not args.generate_raw_md5sums:
-                    continue
-
-                md5sum = calculate_md5(filepath)
-                with open(raw_md5_file, 'a') as f:
-                    f.write(f"{trimmed_path}\t{basename}\t{md5sum}\n")
-                raw_count += 1
-            else:
-                #Generate and save md5sum of processed files always
-                md5sum = calculate_md5(filepath)
-                with open(processed_md5_file, 'a') as f:
-                    f.write(f"{trimmed_path}\t{basename}\t{md5sum}\n")
+            #Generate and save md5sum of processed files always
+            md5sum = calculate_md5(filepath)
+            with open(processed_md5_file, 'a') as f:
+                f.write(f"{trimmed_path}\t{basename}\t{md5sum}\n")
                 processed_count += 1
     
-    if args.generate_raw_md5sums:
-        print(f"Added {raw_count} files to {raw_md5_file}")
     print(f"Added {processed_count} files to {processed_md5_file}")
 
     def dedup_file(filename):
@@ -163,9 +142,6 @@ def main():
                     lines.append(line)
         with open(filename, 'w') as f:
             f.writelines(lines)
-
-    if args.generate_raw_md5sums:
-        dedup_file(raw_md5_file)
     dedup_file(processed_md5_file)
 
 if __name__ == "__main__":
